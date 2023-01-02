9438d1f3a4946c139d7ff948b38446c1351bed9d
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2023-01-02 22:33:02 +0900

    github apiを使わずに処理できるようにしました()

--- main.go
+++ main.go
@@ -6,7 +6,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -17,80 +16,48 @@ import (
 	"strings"
 
 	"github.com/schollz/progressbar/v3"
-	"github.com/tidwall/gjson"
 )
 
 var exename string
 
 func main() {
-	flags := flag.String("t", "", "github token")
-	flag.Parse()
-	req, err := http.NewRequest("GET", "https://api.github.com/repos/Floorp-Projects/Floorp/releases/latest", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Accept", "application/vnd.github+json")
-	if flag.NFlag() == 1 {
-		fmt.Println("set token")
-		req.Header.Add("authorization", "Bearer "+*flags)
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bodyStr := string(body)
-	key := gjson.Get(bodyStr, "assets.#.browser_download_url")
-	key.ForEach(func(key, value gjson.Result) bool {
-		if strings.Contains(value.String(), "floorp-win64.installer") {
-			exename = strings.Split(value.String(), "/")[8]
-			fmt.Printf("[found] %s\n", exename)
-			resp, err := http.Get(value.String())
-			if err != nil {
-				panic(err)
-			}
-			defer resp.Body.Close()
-
-			out, err := os.Create(exename)
-			if err != nil {
-				panic(err)
-			}
-			defer out.Close()
-			bar := progressbar.NewOptions(int(resp.ContentLength),
-				progressbar.OptionSetWriter(os.Stdout),
-				progressbar.OptionEnableColorCodes(true),
-				progressbar.OptionSetDescription("[downloading] "+strings.Split(value.String(), "/")[8]),
-				progressbar.OptionShowBytes(true))
-			io.Copy(io.MultiWriter(out, bar), resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-		return true
-	})
-	resp, err = http.Get("https://www.7-zip.org/a/7zr.exe")
+	value := "https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-win64.installer.exe"
+	exename = strings.Split(value, "/")[8]
+	fmt.Printf("[found] %s\n", exename)
+	resp, err := http.Get(value)
 	if err != nil {
 		panic(err)
 	}
-	out, err := os.Create("7zr.exe")
+
+	out, err := os.Create(exename)
 	if err != nil {
 		panic(err)
 	}
 	bar := progressbar.NewOptions(int(resp.ContentLength),
 		progressbar.OptionSetWriter(os.Stdout),
 		progressbar.OptionEnableColorCodes(true),
-		progressbar.OptionSetDescription("[downloading] 7zr.exe"),
+		progressbar.OptionSetDescription("[downloading] "+exename),
 		progressbar.OptionShowBytes(true))
-	_, err = io.Copy(io.MultiWriter(out, bar), resp.Body)
+	io.Copy(io.MultiWriter(out, bar), resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	resp.Body.Close()
 	out.Close()
+	z7resp, err := http.Get("https://www.7-zip.org/a/7zr.exe")
+	if err != nil {
+		panic(err)
+	}
+	z7out, err := os.Create("7zr.exe")
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(io.MultiWriter(z7out, bar), z7resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	z7resp.Body.Close()
+	z7out.Close()
 
 	exe, err := os.Executable()
 	if err != nil {
